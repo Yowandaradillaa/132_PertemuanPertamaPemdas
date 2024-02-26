7a9e11ead18d7c356e4c8b7bf7037a2cdf739275
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-02-26 11:29:27 +0700

    add display 'Masukan Jejari= '

--- PertemuanPertamaPemdas/PertemuanPertamaPemdas.cpp
+++ PertemuanPertamaPemdas/PertemuanPertamaPemdas.cpp
@@ -3,9 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-
-
+    
+    //begin
+        //numeric nJejari, nLuas
+       //display 'Masukan Jejari= '
+       //accept nJejari
+       // compute nLuas = 3.14 *r *r
+        //display 'Luasnya= ' + nLuas
+        //end
+        
+    double nJejari, nLuas;
+    cout << "Masukan Jejari= ";
     system("pause");
 }
 
